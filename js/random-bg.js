b664fc4fcbab06c81c5ccba337efe53153519219
Zurplupz <76497815+Zurplupz@users.noreply.github.com>
2021-01-13 22:56:28 -0600

    Minor changes on background transition

--- js/random-bg.js
+++ js/random-bg.js
@@ -1,5 +1,20 @@
-bg_choice = ['bg_2','bg_3','bg_4'];
-bg = bg_choice[Math.floor(Math.random() * bg_choice.length)];
-const img = 'url(./img/backgrounds/' + bg + '.jpg)';
+const bg_choice = ['bg_2','bg_3','bg_4'];
 
-$(() => $('.bg').css('background-image', img) );
+$(() => {
+
+	function changeBg() {
+		let current = $('.bg').css('background-image');
+
+		let idx = bg_choice.indexOf(current);
+
+		let new_choices = (idx > -1) ? bg_choice.splice(idx, 1) : bg_choice;
+
+		let bg = new_choices[Math.floor(Math.random() * bg_choice.length)];
+
+		let img = 'url(./img/backgrounds/' + bg + '.jpg)';
+
+		$('.bg').css('background-image', img);
+	}
+
+	changeBg(); 
+});
